f40d5ca41065b8fc421d7cb92a78640337eb4b86
Colter McQuay <colter.mcquay@gmail.com>
2019-03-15 15:24:53 -0700

    Updates to example: - connecting to spotify - added some api buttons

--- example/App.tsx
+++ example/App.tsx
@@ -1,16 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
-
 import React, { useState, useEffect } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { auth, remote, ApiConfig, ApiScope } from 'react-native-spotify-remote';
+import { Platform, StyleSheet, View, FlatList, Alert } from 'react-native';
+import { Button, Text } from 'native-base';
+import { auth, remote, ApiConfig, ApiScope, SpotifyRemoteApi } from 'react-native-spotify-remote';
 
 import {
   SPOTIFY_CLIENT_ID,
@@ -26,6 +17,62 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+
+const SpotifyStatus = (props: { isConnected: boolean }) => {
+  const { isConnected } = props;
+  return isConnected ? (
+    <Text style={styles.connected}>Connected to Spotify!</Text>
+  )
+    : (
+      <Text style={styles.disconnected}>Not Connected to Spotify!</Text>
+    )
+}
+
+const SpotifyControls = (props: { isConnected: boolean,onError:(err:Error)=>void }) => {
+  const { isConnected, onError } = props;
+
+  const actions = [
+    {
+      name:"Play Track",
+      action: async()=>{
+        await remote.playUri("spotify:track:5ckVDfifdJnKEabvjojde4");
+      }
+    },
+    {
+      name: "Play Epic Track",
+      action: async () =>{
+         await remote.playUri("spotify:track:6IA8E2Q5ttcpbuahIejO74");
+         await remote.seek(58000);
+      }
+    },
+    {
+      name: "Pause",
+      action: async () => await remote.pause()
+    }
+  ]
+
+  return (
+    <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', justifyContent: 'center', width: "100%", margin: 10, padding: 10 }}>
+      {actions.map(({ name, action }) => (
+        <Button 
+        disabled={!isConnected} 
+        key={name} 
+        full primary 
+        style={{ margin: 2, height: 70, opacity: isConnected ? 1.0 : 0.3, backgroundColor: isConnected ? undefined : 'gray' }}
+        onPress={async ()=>{
+          try{
+            await action();
+          }catch(err){
+            onError && onError(err);
+          }
+        }}
+        >
+          <Text>{name}</Text>
+        </Button>
+      ))}
+    </View>
+  );
+}
 
 
 interface AppProps {
@@ -44,7 +91,7 @@ const App: React.FunctionComponent<AppProps> = (props) => {
       redirectURL: SPOTIFY_REDIRECT_URL,
       tokenRefreshURL: SPOTIFY_TOKEN_REFRESH_URL,
       tokenSwapURL: SPOTIFY_TOKEN_SWAP_URL,
-      scope: ApiScope.AppRemoteControlScope // Can add more scopes here as flags i.e. Scope1 | Scope2 
+      scope: ApiScope.AppRemoteControlScope // Can add more scopes here as flags i.e. Scope1 | Scope2
     }
 
     // Store handlers in variables so that we can remove them
@@ -63,7 +110,7 @@ const App: React.FunctionComponent<AppProps> = (props) => {
       await remote.connect(token);
     } catch (err) {
       setError(err.message);
-    }    
+    }
 
     // Return our subscription cleanup function
     return () => {
@@ -81,11 +128,8 @@ const App: React.FunctionComponent<AppProps> = (props) => {
       <Text style={styles.welcome}>Welcome to React Native!</Text>
       <Text style={styles.instructions}>{instructions}</Text>
       <Text style={styles.instructions}>{SPOTIFY_REDIRECT_URL}</Text>
-      {isConnected === true ?
-        <Text style={styles.connected}>Connected to Spotify!</Text>
-        : <Text>Not Connected to Spotify!</Text>
-
-      }
+      <SpotifyStatus isConnected={isConnected} />
+      <SpotifyControls isConnected={isConnected} onError={(err)=>setError(err.message)} />
       {error && (
         <Text style={styles.error}>{error}</Text>
       )}
@@ -96,17 +140,26 @@ const App: React.FunctionComponent<AppProps> = (props) => {
 export default App;
 
 const styles = StyleSheet.create({
-  error:{
-    color:"#AA0000",
-    borderColor:"#AA0000",
-    borderWidth:2,
-    fontSize:18,
-    padding:10,
-    margin:10,
-    marginTop:20
+  actionButton: {
+    borderColor: "#0000BB",
+    borderWidth: 2,
+  },
+  error: {
+    color: "#AA0000",
+    borderColor: "#AA0000",
+    borderWidth: 2,
+    fontSize: 18,
+    padding: 10,
+    margin: 10,
+    marginTop: 20
   },
   connected: {
-    color: "#00AA00"
+    color: "#00AA00",
+    fontSize: 16,
+  },
+  disconnected: {
+    color: "#880000",
+    fontSize: 16
   },
   container: {
     flex: 1,
--- example/Readme.md
+++ example/Readme.md
@@ -11,6 +11,11 @@ Which came from https://facebook.github.io/react-native/blog/2018/05/07/using-ty
 
 ## Getting Started
 
+### 0: Install Dependencies
+Run `yarn install` in this directory.
+
+### 1: ENV File
+
 I've also added `react-native-dotenv` for easier configuration.  In order to configure this app you'll need to create a `.env` file in this root directory with the following:
 
 ```env
@@ -23,4 +28,11 @@ SPOTIFY_TOKEN_SWAP_URL="http://{MACHINE_IP_ADDRESS}:3001/swap"
 > Note 1: The last two entries define the auth server endpoints.  You can run the [example auth server](../example-server/README.md) for this purpose.
 
 > Note 2: The `SPOTIFY_REDIRECT_URI` needs to be both registered with your app on the Spotify Dashboard and also needs to be supported by your app.  Instructions for doing this can be found on the [Spotify iOS Quickstart](https://developer.spotify.com/documentation/ios/quick-start/#setup-the-ios-sdk) in the **► Configure Info.plist** section
+
+### 2. Run Server
+You will need to have the [Example Server](../example-server/README.md) running before your app starts up.
+
+### 3:  Start XCode Build
+Open up the `project.pbxproj` in XCode and build it for your phone (note you will need to have Spotify installed on your phone). 
+> This should also start the react native packager as part of the build process if it isn't already running
 
