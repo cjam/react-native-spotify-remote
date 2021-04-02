7c2edc5a9b544b3fa64add03436a29bdd5de67d4
Colter McQuay <colter.mcquay@gmail.com>
2021-04-02 08:19:57 -0700

    Update to example app
    - Provide button for new auth type

--- example/Components/Authenticate.tsx
+++ example/Components/Authenticate.tsx
@@ -2,24 +2,27 @@ import React, { useState, useContext, useCallback } from 'react';
 import { View, Button, Text, Toast, Switch } from 'native-base';
 import styles from '../styles';
 import AppContext from '../AppContext';
+import { Platform } from 'react-native';
+import { ApiConfig } from 'react-native-spotify-remote';
 
 const Authenticate: React.SFC = () => {
     const { isConnected, token, onError, remote, authenticate } = useContext(AppContext)
     const [showDialog, setShowDialog] = useState(false);
     const [autoConnect, setAutoConnect] = useState(true);
 
-    const handleConnect = useCallback((playURI?:string) => {
+    const handleConnect = useCallback((playURI?: string, authType?: ApiConfig["authType"]) => {
         authenticate({
             showDialog,
-            playURI
+            playURI,
+            authType
         });
     }, [showDialog, token])
     return (
         <View style={{ height: 300, display: "flex", justifyContent: "space-evenly", flexDirection: "column" }}>
             <View>
-                <Text style={{fontSize:28, textAlign:"center"}}>Connect To Spotify</Text>
+                <Text style={{ fontSize: 28, textAlign: "center" }}>Connect To Spotify</Text>
             </View>
-            <View style={{display:'flex',flexDirection:"row",justifyContent:'center'}}>
+            <View style={{ display: 'flex', flexDirection: "row", justifyContent: 'center' }}>
                 <Text>Show Auth Dialog:</Text>
                 <Switch value={showDialog} onValueChange={setShowDialog} />
             </View>
@@ -32,6 +35,11 @@ const Authenticate: React.SFC = () => {
             <Button onPress={() => handleConnect("")}>
                 <Text>Auth and Resume Playback</Text>
             </Button>
+            {Platform.OS === "android" && (
+                <Button info onPress={() => handleConnect(undefined, "CODE")}>
+                    <Text>Auth with CODE (android)</Text>
+                </Button>
+            )}
         </View>
     )
 }
